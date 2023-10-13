9d6ea94631fdbc704f254d57530e324fbdc26435
Andrea Moleri <andreamoleri@gmail.com>
2023-10-13 20:39:56 +0200

    Added Formatting, Comments and Docstrings

--- hauntings.py
+++ hauntings.py
@@ -1,53 +1,44 @@
-# Libraries Import
-import pandas as pd
+# Import necessary libraries for data visualization, map plotting, and analysis
 import folium
+import pandas as pd
 from folium.plugins import HeatMap
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-import matplotlib.pyplot as plt
 
-# Load the CSV file
+# Define the path to the CSV containing haunted locations data
+# Then read the data from the CSV file into a Pandas DataFrame
 file_path = 'hauntings.csv'
-df = pd.read_csv(file_path)
+dataframe = pd.read_csv(file_path)
 
-def explore_data_frame():
-   # Explore the data
-    print(df.head())  # Display the first rows of the DataFrame
-    print(df.info())  # Display information about the DataFrame
-     
-# Conta le occorrenze di ciascuno stato nel DataFrame
-state_counts = df['state'].value_counts().reset_index()
-state_counts.columns = ['state', 'count']
+# Convert latitude and longitude columns to numeric, handling errors by coercing
+dataframe['latitude'] = pd.to_numeric(dataframe['latitude'], errors='coerce')
+dataframe['longitude'] = pd.to_numeric(dataframe['longitude'], errors='coerce')
 
-# Converte 'latitude' e 'longitude' in numeri (float) se non lo sono già
-df['latitude'] = pd.to_numeric(df['latitude'], errors='coerce')
-df['longitude'] = pd.to_numeric(df['longitude'], errors='coerce')
-
-# Crea una mappa centrata negli Stati Uniti
+# Create a base map centered around the United States of America
 us_map = folium.Map(location=[37.0902, -95.7129], zoom_start=4)
 
-
-# Crea una lista di coordinate [(lat, lon, peso)] per la Heatmap
+# Prepare data for a heatmap visualization
 heat_data = []
-for index, row in df.iterrows():
-    
-    
-    # Controllo se il valore nella colonna "latitude" è nullo prima di accedervi
-    if not pd.isnull(df.at[index, 'latitude']):
-        latitude = df.at[index, 'latitude']
+for index, row in dataframe.iterrows():
+    if not pd.isnull(dataframe.at[index, 'latitude']):
+        latitude = dataframe.at[index, 'latitude']
     else:
-        latitude = 100
-        
-    if not pd.isnull(df.at[index, 'longitude']):
-        longitude = df.at[index, 'longitude']
-    else:
-        longitude = 100
-    
-    heat_data.append([latitude, longitude, index])
+        latitude = 0  # Replace missing latitude with an arbitrary value
 
-# Aggiungi la Heatmap alla mappa
+    if not pd.isnull(dataframe.at[index, 'longitude']):
+        longitude = dataframe.at[index, 'longitude']
+    else:
+        longitude = 0  # Replace missing longitude with an arbitrary value
+
+    heat_data.append([latitude, longitude, index])  # Append every row to the heatmap
+
+# Create a heatmap using the prepared data
+# Then save the map as an HTML file
 HeatMap(heat_data).add_to(us_map)
+us_map.save('HauntingsDistribution.html')
 
-# Salva la mappa in un file HTML
-us_map.save('us_state_distribution.html')
-print(len(heat_data))
+# Function to explore the DataFrame, providing a summary of its contents
+def explore_data_frame():
+    """
+    Display the first few rows of the DataFrame and print information about the DataFrame's structure.
+    """
+    print(dataframe.head())
+    print(dataframe.info())
