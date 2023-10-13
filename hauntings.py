654aa7447bd4d9a0552ed39ac2d2337809f9b85b
Andrea Moleri <andreamoleri@gmail.com>
2023-10-13 19:45:50 +0200

    Implemented HTML Map System

--- hauntings.py
+++ hauntings.py
@@ -1,21 +1,29 @@
 # Libraries Import
 import pandas as pd
+import folium
 import geopandas as gpd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
 
 # Load the CSV file
 file_path = 'hauntings.csv'
 df = pd.read_csv(file_path)
 
-# Split the data into features (X) and target (y)
-X = df.drop('target_column_name', axis=1)  # Replace 'target_column_name' with the name of the target column
-y = df['target_column_name']
-
 def explore_data_frame():
    # Explore the data
     print(df.head())  # Display the first rows of the DataFrame
-    print(df.info())  # Display information about the DataFrame 
+    print(df.info())  # Display information about the DataFrame
+     
+# Conta le occorrenze di ciascuno stato nel DataFrame
+state_counts = df['state'].value_counts().reset_index()
+state_counts.columns = ['state', 'count']
 
+# Crea una mappa centrata negli Stati Uniti
+us_map = folium.Map(location=[37.0902, -95.7129], zoom_start=4)
+
+
+# Salva la mappa in un file HTML
+us_map.save('us_state_distribution.html')
